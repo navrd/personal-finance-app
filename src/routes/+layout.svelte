e3832ce6b2ed5a3b34a8cca3c7512ba71fbe6582
navrd <navrd@yandex.ru>
2025-07-12 21:28:02 +0300

    Added routing and sass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    
+	let { children } = $props();
+</script>
+
+<main>
+	<h1>layout</h1>
+	<a href="/budgets">budgets</a>
+	<a href="/transactions">transactions</a>
+	<a href="/reccuring">reccuring</a>
+	<a href="/pots">pots</a>
+	<a href="/overview">overview</a>
+    {@render children()}
+</main>
