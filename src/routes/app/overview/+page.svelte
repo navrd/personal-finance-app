0f665e05bf5fc6e26a5de01536cb06b30f2a40c1
navrd <navrd@yandex.ru>
2025-07-28 22:22:59 +0300

    Added transactions, reccuring bills and pots into overview +page

--- src/routes/app/overview/+page.svelte
+++ src/routes/app/overview/+page.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 	import { BudgetsDiagram } from '$lib/components';
-	import type { Balance } from '$lib/types';
+	import { sortTransactions } from '$lib/helpers/transactions';
+	import type { Balance, Pot, Transaction, TransactionSortOption } from '$lib/types';
 	import { getContext } from 'svelte';
 
-	let balance: () => Balance = getContext('balance');
-	$inspect(balance())
+	let transactionSortOptions: TransactionSortOption[] = getContext('transactionSortOptions');
 
+	let balance: () => Balance = getContext('balance');
 	let balanceData = $derived.by<Pick<Balance, 'current' | 'expenses' | 'income'>>(() => {
 		return (({ current, expenses, income }) => ({ current, expenses, income }))(balance());
 	});
-	$inspect(balanceData)
+
+	let pots: () => Pot[] = getContext('pots');
+	let nonEmptyPots = $derived.by(() => {
+		return pots().filter((pot) => pot.total > 0);
+	});
+	let totalSavigs = $derived(nonEmptyPots.reduce((acc, { total }) => acc + total, 0));
+
+	let transactions: () => Transaction[] = getContext('transactions');
+	let transactionsSortedByDate = $derived.by(() => {
+		return sortTransactions(transactions(), transactionSortOptions[4].id, transactionSortOptions);
+	});
+	let reccuringTransactions = $derived(
+		transactionsSortedByDate.filter((transaction) => transaction.recurring)
+	);
 </script>
 
 <svelte:head>
-	<title>Personal Finance App - Home</title>
+	<title>Personal Finance App - Overview</title>
 	<meta name="description" content="Personal Finance App" />
 </svelte:head>
 
@@ -25,4 +39,37 @@
 			<p class="balance-entry__value">{value}</p>
 		</div>
 	{/each}
+</div>
+<div class="pots">
+	<h2 class="pots__total-savings">{totalSavigs}</h2>
+	<a href="/app/pots">details</a>
+	{#each pots() as pot}
+		<div class="pot">
+			<h3 class="pot__title">{pot.name}</h3>
+			<p class="pot__total">{pot.total}/{pot.target}</p>
+		</div>
+	{/each}
+</div>
+<div class="transactions">
+	<h2>Transactions</h2>
+	<a href="/app/transactions">details</a>
+	<ul class="transactions-list">
+		{#each transactionsSortedByDate.slice(0, 5) as transaction}
+			<li class="transaction">
+				<p>{transaction.name} {transaction.amount} {transaction.date}</p>
+			</li>
+		{/each}
+	</ul>
+</div>
+
+<div class="transactions">
+	<h2>Reccuring Transactions</h2>
+	<a href="/app/reccuring">details</a>
+	<ul class="transactions-list">
+		{#each reccuringTransactions.slice(0, 5) as transaction}
+			<li class="transaction">
+				<p>{transaction.name} {transaction.amount} {transaction.date}</p>
+			</li>
+		{/each}
+	</ul>
 </div>
